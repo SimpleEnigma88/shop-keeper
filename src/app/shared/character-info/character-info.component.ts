f4cae53ccae85302b612f078a0466469f818272e
Robert Andrew Moore <ra.moore573@gmail.com>
2024-03-18 22:15:56 -0500

    Update app layout and character comp, class Icon CSS

--- src/app/shared/character-info/character-info.component.ts
+++ src/app/shared/character-info/character-info.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './character-info.component.css'
 })
 export class CharacterInfoComponent {
+  images = ['artificer.jpeg', 'bard.jpg', 'cleric.jpeg', 'druid.jpg', 'fighter.jpg', 'monk.jpg', 'ranger.jpg', 'warlock.jpg', 'barbarian.jpeg', 'wizard.jpeg', 'rogue.jpeg', 'paladin.jpeg',];
+
+  randomImage = this.images[Math.floor(Math.random() * this.images.length)];
 
 }
