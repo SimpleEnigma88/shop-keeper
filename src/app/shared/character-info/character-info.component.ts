98c83fca7d4c90aee24a942d87a54c8e01340cf2
Robert Andrew Moore <ra.moore573@gmail.com>
2024-03-21 20:39:11 -0500

    Update app component and navbar styles

--- src/app/shared/character-info/character-info.component.ts
+++ src/app/shared/character-info/character-info.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-character-info',
@@ -7,9 +7,23 @@ import { Component } from '@angular/core';
   templateUrl: './character-info.component.html',
   styleUrl: './character-info.component.css'
 })
-export class CharacterInfoComponent {
+
+export class CharacterInfoComponent implements OnInit {
   images = ['artificer.jpeg', 'bard.jpg', 'cleric.jpeg', 'druid.jpg', 'fighter.jpg', 'monk.jpg', 'ranger.jpg', 'warlock.jpg', 'barbarian.jpeg', 'wizard.jpeg', 'rogue.jpeg', 'paladin.jpeg',];
+  constructor() {
+    this.randomClass = '';
+  }
+  randomImage: string = '';
+  randomClass: string = '';
 
-  randomImage = this.images[Math.floor(Math.random() * this.images.length)];
+  getRandomNumber() {
+    return Math.floor(Math.random() * this.images.length);
+  }
 
+  ngOnInit() {
+    this.randomImage = this.images[Math.floor(Math.random() * this.images.length)];
+    this.randomClass = this.randomImage.split('.')[0];
+    this.randomClass = this.randomClass.charAt(0).toUpperCase() + this.randomClass.slice(1);
+    console.log(this.randomImage);
+  }
 }
