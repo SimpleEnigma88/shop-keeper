e2759cbd74a2724377e842e7e33b87648753aa7f
Robert Andrew Moore <ra.moore573@gmail.com>
2024-04-02 18:27:11 +0000

    Add Font Family

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
 import { TreasureComponent } from './shared/treasure/treasure.component';
 import { HomeComponent } from './home/home.component';
 
