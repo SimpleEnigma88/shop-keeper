6743276164d3b07eed28988dd520497a74741336
Robert Andrew Moore <ra.moore573@gmail.com>
2024-04-06 19:53:12 -0500

    Update routing in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
-import { CharacterMagicItemInventoryComponent } from './shared/character-magic-item-inventory/character-magic-item-inventory.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     { path: 'default', component: HeaderComponent, outlet: 'headerView' },
-    { path: 'inventory', component: HomeComponent, outlet: 'mainView' },
-    { path: '', redirectTo: '/(headerView:default//mainView:inventory)', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent, outlet: 'mainView' },
+    { path: '', redirectTo: '/(headerView:default//mainView:home)', pathMatch: 'full' },
     { path: '', component: AppComponent },
 ];
